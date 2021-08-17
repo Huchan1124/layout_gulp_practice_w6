b9f3e70e087fd45ab7bfa2b8c2dcdba61a2e8abd
Huchan1124 <garlic1124@gmail.com>
2021-08-18 00:04:20 +0800

    完成liveReview recommend PC區塊

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -10,3 +10,15 @@
   
 // });
 
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+      spaceBetween: 30,
+      breakpoints: {
+        768:{
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }
+  });
