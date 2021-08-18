e61d0c8cae389cf5b0347e1e83690e9c9f5265d8
Huchan1124 <garlic1124@gmail.com>
2021-08-18 11:01:26 +0800

    完成contact PC區塊

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,16 +1,4 @@
 
-// $(document).ready(function () {
-//   $(".nav-link").hover(function () {
-//       $(this).addClass("nav-link--underline");
-      
-//     }, function () {
-//       $(this).removeClass("nav-link--underline");
-//     }
-//   );
-  
-// });
-
-
 
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
